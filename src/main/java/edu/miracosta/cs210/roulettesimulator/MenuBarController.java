221eb97a38a2cfd03ce92b57b82b61d31fd68d8a
aguilaraul <raula@live.com>
2021-11-13 23:57:05 -0800

    cleanup stats window Also added uml diagram

--- src/main/java/edu/miracosta/cs210/roulettesimulator/MenuBarController.java
+++ src/main/java/edu/miracosta/cs210/roulettesimulator/MenuBarController.java
@@ -3,10 +3,12 @@ package edu.miracosta.cs210.roulettesimulator;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 
+import java.io.IOException;
+
 public class MenuBarController {
 
     @FXML
-    protected void onStatsMenuClick() {
+    protected void onStatsMenuClick() throws IOException {
         StatsWindow.display();
     }
 
--- src/main/java/edu/miracosta/cs210/roulettesimulator/StatsWindow.java
+++ src/main/java/edu/miracosta/cs210/roulettesimulator/StatsWindow.java
@@ -1,41 +1,39 @@
 package edu.miracosta.cs210.roulettesimulator;
 
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class StatsWindow {
-    public static void display() {
-        // Window settings
-        Stage window = new Stage();
+    static private Stage window;
+
+    @FXML
+    Label gamesWonValue;
+    @FXML
+    Label currentBalanceValue;
+
+    @FXML
+    protected void onCloseButtonClick() {
+        window.close();
+    }
+
+    public static void display() throws IOException {
+        window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle("Stats");
-        window.setMinWidth(250);
-        window.setMinHeight(250);
-
-        // Stats
-        Label gamesWon   = new Label("Games Won:");
-        Label gamesLost  = new Label("Games Lost:");
-        Label winPercent = new Label("Win Percentage:");
-        Label amtBet     = new Label("Bet Total:");
-        Label amtWon     = new Label("Total Earnings:");
-        Label amtLost    = new Label("Total Lost:");
-
-        // Close button
-        Button closeButton = new Button("Close");
-        closeButton.setOnAction(e -> window.close());
-
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(gamesWon, gamesLost, winPercent, amtBet, amtWon, amtLost);
-        layout.getChildren().add(closeButton);
-        layout.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(layout);
-        window.setScene(scene);
+        FXMLLoader statsSceneLoader = new FXMLLoader(App.class.getResource("statsWindow-view.fxml"));
+        Scene statsScene = new Scene(statsSceneLoader.load(), 250, 250);
+        window.setScene(statsScene);
         window.showAndWait();
+    }
+
+    private void setCurrentBalanceValue(double value) {
+        String balance = String.valueOf(value);
+        currentBalanceValue.setText(balance);
     }
 }
