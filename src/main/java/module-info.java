068a770331bb96eb3f1878e8f4540d470469ef97
aguilaraul <raula@live.com>
2021-11-30 21:41:38 -0800

    added .gitignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module edu.miracosta.cs210.roulettesimulator {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+    //requires org.controlsfx.controls;
+    //requires com.dlsc.formsfx;
 
     opens edu.miracosta.cs210.roulettesimulator to javafx.fxml;
     exports edu.miracosta.cs210.roulettesimulator;
