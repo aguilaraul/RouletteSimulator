9d2f757627d85997d8f4696387ed0275b3b6558c
Robert Edmonston <bedmonston@icloud.com>
2021-12-06 18:11:57 -0800

    Removed commented out code

--- src/main/java/edu/miracosta/cs210/roulettesimulator/App.java
+++ src/main/java/edu/miracosta/cs210/roulettesimulator/App.java
@@ -25,18 +25,6 @@ public class App extends Application {
         if (spinList == null) {
             ArrayList<Integer> spinArray = new ArrayList<Integer>();
             spinList = FXCollections.observableList(spinArray);
-//            spinList.addListener(new ListChangeListener() {
-//                    @Override
-//                    public void onChanged(ListChangeListener.Change change) {
-//                        System.out.println("Detected a change! ");
-//                        while (change.next()) {
-//                            System.out.println("Was added? " + change.wasAdded());
-//                            System.out.println("Was removed? " + change.wasRemoved());
-//                            System.out.println("Was replaced? " + change.wasReplaced());
-//                            System.out.println("Was permutated? " + change.wasPermutated());
-//                        }
-//                    }
-//            });
         }
         return spinList;
     }
