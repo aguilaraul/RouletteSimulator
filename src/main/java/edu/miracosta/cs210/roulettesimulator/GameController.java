845b1c972ac792f3c98a14d92304f258332f5f38
aguilaraul <raula@live.com>
2021-12-14 13:39:32 -0800

    check if valid wager
    checking if valid wager before accepting bet

--- src/main/java/edu/miracosta/cs210/roulettesimulator/GameController.java
+++ src/main/java/edu/miracosta/cs210/roulettesimulator/GameController.java
@@ -63,17 +63,36 @@ public class GameController {
 
     @FXML
     protected void onBetClick() throws IOException {
-        getWagerAmount();
-        log.getItems().add("You bet " + text.formattedBalance(this.wager));
-        App.changeScene("Spin #" + spinCount++, "roulette-wheel-view.fxml", "css/game.css");
+        checkBet();
     }
 
-    private void getWagerAmount() {
-        try {
-            this.wager = Integer.parseInt(wagerTextField.getText());
-        } catch (NumberFormatException e) {
-            log.getItems().add("Please enter a wager amount");
+    /**
+     * Checks if valid bet was placed
+     * @throws IOException unable to change scene
+     */
+    private void checkBet() throws IOException {
+        if(checkWager()) {
+            log.getItems().add("You bet " + text.formattedBalance(this.wager));
+            App.changeScene("Spin #" + spinCount++, "roulette-wheel-view.fxml", "css/game.css");
         }
+    }
+
+    private boolean checkWager() {
+        boolean validWager = false;
+        if (wagerTextField.getText().isBlank() || wagerTextField.getText().equals("0")) {
+            log.getItems().add("Enter a wager amount");
+        } else {
+            try {
+                this.wager = Double.parseDouble(wagerTextField.getText());
+                if(wager > this.balance) {
+                    log.getItems().add("Your wager is greater than your balance");
+                }
+                validWager = wager > 0 && wager <= this.balance;
+            } catch (NumberFormatException exception) {
+                log.getItems().add("Please enter a valid wager");
+            }
+        }
+        return validWager;
     }
 
     @FXML
