49ba5d432cf696145002a2e302857fbb740d1388
Bob Edmonston <bedmonston@mac.com>
2021-12-15 18:13:50 -0800

    Added new test isLoser

--- src/main/test/java/edu/miracosta/cs210/roulettesimulator/NumberBetTest.java
+++ src/main/test/java/edu/miracosta/cs210/roulettesimulator/NumberBetTest.java
@@ -9,6 +9,18 @@ class NumberBetTest {
     @Test
     void isWinner() {
         NumberBet win = new NumberBet();
+        win.setNumber(1);
+        boolean result = true;
+        boolean winner;
+        int bet = 1;
+        winner = win.isWinner(bet);
+        assertEquals(result, winner);
+    }
+
+    @Test
+    void isLoser() {
+        NumberBet win = new NumberBet();
+        win.setNumber(2);
         boolean result = false;
         boolean winner;
         int bet = 1;
